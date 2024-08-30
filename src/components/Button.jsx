325ddc664f251b61f635fb6609f60fb999a8971c
CPCayson <cpcayson@gmail.com>
2024-08-30 18:08:46 -0500

    cool

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ onClick, className, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 rounded-full ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 dark:bg-gray-900 text-center p-2 text-sm text-gray-600 dark:text-gray-400">
+      © 2024 RideShare App
+    </footer>
+  );
+};
+
+export default Footer;
